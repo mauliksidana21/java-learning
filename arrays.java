53fb3f8f3f3d2ead8c9042efa4f87a6c06c5d1e0
mauliksidana21 <mauliksidana@gmail.com>
2026-07-25 22:46:54 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String []args ){
+        int arr[];     //declaration of array`
+
+        arr = new int[5];
+
+        int brr[] = new int[]{10,20,30}; // declaration with initialization
+
+        System.out.println("value at 0th index " + brr[0]);
+        System.out.println("value at 1st index " + brr[1]);
+
+
+
+    }
+}
