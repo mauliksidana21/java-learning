06cc5061b6c32a86dbe88d260e98899725c864a3
mauliksidana21 <mauliksidana@gmail.com>
2026-07-25 23:12:23 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -9,6 +9,19 @@ public class arrays {
         System.out.println("value at 0th index " + brr[0]);
         System.out.println("value at 1st index " + brr[1]);
         System.out.println("value at 2nd index " + brr[2]);
+        //accesing array elements :
+        System.out.println("value at 0th index " + brr[0]);
+        System.out.println("value at 1st index " + brr[1]);
+        System.out.println("value at 2nd index " + brr[2]);
+// uysing for loop
+        for (int i=0;i<3;i++){
+            System.out.println("array is -> "+brr[i]);
+        }
+        // or we can also use length function
+        int n = brr.length;
+        for(int i=0;i<n;i++){
+            System.out.println("array is -> "+brr[i]);
+        }
 
 
 
