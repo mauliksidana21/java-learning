58d594ded5fa69d8ec69b01a7588209fc3c987a9
mauliksidana21 <mauliksidana@gmail.com>
2026-07-25 23:05:34 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -5,7 +5,7 @@ public class arrays {
         arr = new int[5];
 
         int brr[] = new int[]{10,20,30}; // declaration with initialization
-
+//accesing array elements :
         System.out.println("value at 0th index " + brr[0]);
         System.out.println("value at 1st index " + brr[1]);
         System.out.println("value at 2nd index " + brr[2]);
